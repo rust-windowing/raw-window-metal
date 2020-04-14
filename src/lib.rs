2cc4647178c69aaf34e7fbe0a480b5ddec261a51
msiglreith <m.siglreith@gmail.com>
2020-04-14 16:53:50 +0200

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+use objc::runtime::Object;
+
+#[cfg(target_os = "macos")]
+pub mod macos;
+#[cfg(target_os = "ios")]
+pub mod ios;
+
+pub type CAMetalLayer = *mut Object;
+
+pub enum Layer {
+    Existing(CAMetalLayer),
+    Allocated(CAMetalLayer),
+    None,
+}
---
+++ src/macos.rs
@@ -0,0 +1,63 @@
+use crate::{CAMetalLayer, Layer};
+use objc::{msg_send, runtime::{Object, BOOL, YES}};
+use raw_window_handle::macos::MacOSHandle;
+use std::mem;
+
+///
+pub unsafe fn metal_layer_from_handle(handle: MacOSHandle) -> Layer {
+    if !handle.ns_view.is_null() {
+        metal_layer_from_ns_view(handle.ns_view)
+    } else if !handle.ns_window.is_null() {
+        metal_layer_from_ns_window(handle.ns_window)
+    } else {
+        Layer::None
+    }
+}
+
+///
+pub unsafe fn metal_layer_from_ns_view(view: *mut c_void) -> Layer {
+    // Check if the view is a CAMetalLayer
+    let class = class!(CAMetalLayer);
+    let is_actually_layer: BOOL = msg_send![view, isKindOfClass: class];
+    if is_actually_layer == YES {
+        return Layer::Existing(view);
+    }
+
+    // Check if the view contains a valid CAMetalLayer
+    let existing: CAMetalLayer = msg_send![view, layer];
+    let use_current = if existing.is_null() {
+        false
+    } else {
+        let result: BOOL = msg_send![existing, isKindOfClass: class];
+        result == YES
+    };
+
+    let render_layer: CAMetalLayer = if use_current {
+        Layer::Existing(existing)
+    } else {
+        // Allocate a new CAMetalLayer for the current view
+        let layer: CAMetalLayer = msg_send![class, new];
+        let () = msg_send![view, setLayer: layer];
+        let () = msg_send![view, setWantsLayer: YES];
+        let bounds: CGRect = msg_send![view, bounds];
+        let () = msg_send![layer, setBounds: bounds];
+
+        let window: cocoa::base::id = msg_send![view, window];
+        if !window.is_null() {
+            let scale_factor: CGFloat = msg_send![window, backingScaleFactor];
+            let () = msg_send![layer, setContentsScale: scale_factor];
+        }
+
+        Layer::Allocated(layer)
+    };
+
+    let _: *mut c_void = msg_send![view, retain];
+    render_layer
+}
+
+///
+pub unsafe fn metal_layer_from_ns_window(window: *mut c_void) -> CAMetalLayer {
+    let ns_window = window as *mut Object;
+    let ns_view = msg_send![ns_window, contentView];
+    metal_layer_from_ns_view(ns_view)
+}
