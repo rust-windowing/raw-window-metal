0f7971c853e3d28b10aa0d87db77accfe413f754
Marijn Suijten <marijn@traverseresearch.nl>
2022-07-28 22:33:38 +0200

    Ignore `clippy::let-unit-value` just like `winit`
    It seems there's no pattern of `let _: ()`/`let (): ()`/`let (): _`
    binding that circumnavigates this clippy warning: ignore it instead.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 #![cfg(any(target_os = "macos", target_os = "ios"))]
-#![allow(clippy::missing_safety_doc)]
+#![allow(clippy::missing_safety_doc, clippy::let_unit_value)]
 
 #[macro_use]
 extern crate objc;
