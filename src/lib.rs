a89ea67e350b46414474be132d80e17da151c0d8
msiglreith <m.siglreith@gmail.com>
2023-10-31 19:24:27 +0100

    Restrict targets to macos and ios

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-// #![cfg(any(target_os = "macos", target_os = "ios"))]
+#![cfg(any(target_os = "macos", target_os = "ios"))]
 #![allow(clippy::missing_safety_doc, clippy::let_unit_value)]
 
 #[macro_use]
